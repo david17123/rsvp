97fc515d0036c0e845eeca0f54e6c7906e8609d5
David Andika <david@dotdev.com.au>
2020-01-27 16:33:01 +1100

    Added not-yet-working menu on admin

--- hosting/src/components/TopBar.tsx
+++ hosting/src/components/TopBar.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 import AppBar from '@material-ui/core/AppBar'
+import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
+import Drawer from '@material-ui/core/Drawer'
 import IconButton from '@material-ui/core/IconButton'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
+import AssignmentIcon from '@material-ui/icons/Assignment'
 import MenuIcon from '@material-ui/icons/Menu'
+import PeopleIcon from '@material-ui/icons/People'
 
 import { logout } from '../services/login'
 
@@ -25,19 +33,45 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export default function TopBar(props: TopBar.Props) {
   const classes = useStyles(props)
+  const [drawerOpen, setDrawerOpen] = React.useState(false)
 
   return (
-    <AppBar position="static" className={classes.root}>
-      <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          RSVP dashboard
-        </Typography>
-        <Button color="inherit" onClick={() => logout()}>Logout</Button>
-      </Toolbar>
-    </AppBar>
+    <React.Fragment>
+      <AppBar position="static" className={classes.root}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setDrawerOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            RSVP dashboard
+          </Typography>
+          <Button color="inherit" onClick={() => logout()}>Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <Box
+          width={250}
+          role="presentation"
+        >
+          <List>
+            <ListItem button>
+              <ListItemIcon><PeopleIcon /></ListItemIcon>
+              <ListItemText primary="Guests" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><AssignmentIcon /></ListItemIcon>
+              <ListItemText primary="Bookings" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </React.Fragment>
   )
 }
 
